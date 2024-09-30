b14e64fe6a31e1f6c739f3eb1618ceea6495b36c
crh1987 <crh1987@rit.edu>
2024-09-30 17:07:11 -0400

    Finished Unity hand tracking, need to test with quest

--- Assets/touchme.cs
+++ Assets/touchme.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class touchme : MonoBehaviour
 {
+    public Light light1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,20 @@ public class touchme : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "RightIndex")
+        {
+            if (light1.enabled == false)
+            {
+                light1.enabled = true;
+            }
+            else
+            {
+                light1.enabled = true;
+            }
+        }
     }
 }
